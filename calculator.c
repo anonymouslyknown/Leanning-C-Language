24e7b1e3da0f1b4619741f961b3ae40569dad967
Saloni <salgoyal@adobe.com>
2019-05-31 14:04:42 +0530

    edit calculator.c

---
+++ calculator.c
@@ -0,0 +1,35 @@
+    #include<stdio.h>
+    int main()
+
+{
+    float a,b,c;
+    int d;
+    printf("enter first number ");
+    scanf("%f",&a);
+    printf("press \n 1 for addition \n 2 for subtraction \n 3 for division \n 4 for mutiplication ");
+    scanf("%d",&d);
+    printf("enter second number ");
+    scanf("%f",&b);
+    if (d==1) {
+        c=a+b;
+        }
+
+    else if (d==2) {
+        c=a-b;
+    }
+    else if (d==3){
+        while(b==0){
+            printf("Please enter number other than 0 ");
+            scanf("%f",&b);
+        }
+        c=a/b;
+    }
+    else if (d==4){
+        c=a*b;
+    }
+    // else{
+    //     c=a%b;
+    // }
+    printf("the answer is %f \n",c);
+
+}
