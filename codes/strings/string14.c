260abda89641504ffbd881cedc85e9fb790470da
gopal goyal <51030583+buggyman911@users.noreply.github.com>
2019-07-12 18:32:54 +0530

    somecodes edited

--- codes/strings/string14.c
+++ codes/strings/string14.c
@@ -2,5 +2,39 @@
 #include<stdio.h>
 void main()
 {
+    char a[100];
+    int i,z;
+    printf("enter string element ");
+    scanf("%[^\n]",a);
+    for(i=0;a[i]!='\0';i++)
+    {
 
+    }
+    for(;a[i]!=' ';i--)
+    {
+
+    }
+    z=i+1;
+    for(;a[z]!='\0';z++)
+    {
+        printf("%c",a[z]);
+    }
+    printf(" ");
+    i--;
+    for(;i>=1;i--)
+    {
+        if(a[i]==' ')
+        {
+            z=i+1;
+            for(;a[z]!=' ';z++)
+            {
+                printf("%c",a[z]);
+            }
+            printf(" ");
+        }
+    }
+    for(;a[i]!=' ';i++)
+    {
+        printf("%c",a[i]);
+    }
 }
