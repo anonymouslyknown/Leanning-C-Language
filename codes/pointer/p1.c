03c7545f588a5e785ed5d65744d801cbccab7adf
Qwerty-123-debug <63785291+Qwerty-123-debug@users.noreply.github.com>
2020-10-08 20:59:50 +0530

    Update p1.c

--- codes/pointer/p1.c
+++ codes/pointer/p1.c
@@ -2,15 +2,18 @@
 #include<stdio.h>
 void main()
 {
-    int a,b,z,*p1,*p2,*p3;
+    int a,b,z,w,*p1,*p2,*p3,*p4;
     p1=&a;
     p2=&b;
     p3=&z;
+    p4=&w;
     printf("enter first number \n");
     scanf("%d",p1);
     printf("enter second number ");
     scanf("%d",p2);
     *p3=*p2+*p1;
+    *p4=*p3+*p1;
     printf("%d",*p3);
+    printf("%d",*p4);
 
 }
